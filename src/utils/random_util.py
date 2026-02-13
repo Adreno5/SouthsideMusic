287b06b432bb120e580c87c81892fb24e8eada50
Adreno5 <3855904186@qq.com>
2026-02-13 23:08:06 +0800

    Revert "feat(main): 添加调试窗口和日志记录功能"
    This reverts commit 397824d9204b1c9d8477479180393771e808c88d.

--- src/utils/random_util.py
+++ src/utils/random_util.py
@@ -11,7 +11,6 @@ class AdvancedRandom(Generic[T]):
         self.list_weight: list[float] = []
         self.randomed_times: list[int] = []
         self.target_lst: list[T] = []
-        self.adjusted_weights = []
 
     def init(self, lst: list[T]):
         if not lst:
@@ -24,33 +23,33 @@ class AdvancedRandom(Generic[T]):
     
     def random(self) -> T:
         total_weight = 0
-        self.adjusted_weights = []
+        adjusted_weights = []
         
         for i, times in enumerate(self.randomed_times):
             weight = self.list_weight[i] / (times + 1)
-            self.adjusted_weights.append(weight)
+            adjusted_weights.append(weight)
             total_weight += weight
         
         if total_weight <= 0:
             total_weight = len(self.target_lst)
-            self.adjusted_weights = [1.0 for _ in self.target_lst]
+            adjusted_weights = [1.0 for _ in self.target_lst]
         
         randomed = random.random() * total_weight
         current = 0
         
-        for i, weight in enumerate(self.adjusted_weights):
+        for i, weight in enumerate(adjusted_weights):
             current += weight
             if randomed <= current:
                 selected_item = self.target_lst[i]
                 self.randomed_times[i] += 1
                 logging.info(f'randomed {selected_item} times {self.randomed_times[i]}')
                 logging.debug(self.randomed_times)
-                logging.debug(self.adjusted_weights)
+                logging.debug(adjusted_weights)
                 return selected_item
         
         selected_item = self.target_lst[-1]
         self.randomed_times[-1] += 1
         logging.info(f'randomed {selected_item} times {self.randomed_times[-1]}')
         logging.debug(self.randomed_times)
-        logging.debug(self.adjusted_weights)
+        logging.debug(adjusted_weights)
         return selected_item

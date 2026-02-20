d8aa0a49408395aed07a6201eee444f7d4bbdf46
Adreno5 <3855904186@qq.com>
2026-02-20 20:22:50 +0800

    build(build): 更新构建脚本以正确复制ffmpeg文件
    修改了build.bat脚本，将ffmpeg目录复制到main.dist目录下，
    确保应用程序能够正确访问ffmpeg资源文件

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 title Building
 
-nuitka src/main.py --standalone --enable-plugin=pyside6,numpy --windows-console-mode=hide --output-filename=SouthsideMusic --include-data-dir="./icons=./icons" --include-data-dir="./fonts=./fonts" --include-data-dir="./ffmpeg=./ffmpeg"
+nuitka src/main.py --standalone --enable-plugin=pyside6,numpy --windows-console-mode=hide --output-filename=SouthsideMusic --include-data-dir="./icons=./icons" --include-data-dir="./fonts=./fonts"
+copy ffmpeg\ main.dist\ffmpeg\
 
 echo Done!
 title Done

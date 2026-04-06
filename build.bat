2ac8780fcf014bfb42d05ce2ebbad78e1b6106e9
Adreno5 <3855904186@qq.com>
2026-04-06 14:43:23 +0800

    build(build): 修复Windows构建脚本并更新版本号
    - 在build.bat中添加call命令确保虚拟环境激活和Nuitka构建正确执行
    - 更新pyproject.toml中的版本号从v13到v15
    - 保持命令调用的一致性以确保构建流程正常工作

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 title Building
 
-.venv\Scripts\activate
-nuitka src/main.py --standalone --enable-plugin=pyside6,numpy --windows-console-mode=hide --output-filename=SouthsideMusic --include-data-dir="./icons=./icons" --include-data-dir="./fonts=./fonts"`
+call .venv\Scripts\activate
+call nuitka src/main.py --standalone --enable-plugin=pyside6,numpy --windows-console-mode=hide --output-filename=SouthsideMusic --include-data-dir="./icons=./icons" --include-data-dir="./fonts=./fonts"
+
 mkdir .\main.dist\ffmpeg 2>nul
 xcopy .\ffmpeg .\main.dist\ffmpeg /E /I /Y
 

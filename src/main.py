9383911e6c98ef54079dc69b22f0e14dc4d55f81
Adreno5 <3855904186@qq.com>
2026-05-16 17:11:51 +0800

    updated to v29

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 from __future__ import annotations
-
-import logging
-
-import os
 import sys
-
-from views.playlist_page import PlaylistPage
-from views.setting_page import SettingPage
+import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'views'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'services'))
 sys.path.append(os.path.dirname(__file__))
+
+import logging
+
+from views.playlist_page import PlaylistPage
+from views.setting_page import SettingPage
 
 import threading
 import time
